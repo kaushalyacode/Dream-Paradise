7d9c838802b94792ee438c44a528d7480967a613
Kaushalya Harshanath <wakharshanath@gmail.com>
2021-08-15 19:21:46 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>QA-Dream-paradise</title>
+  </head>
+  <body>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-primary ">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white" href="#">Dream-Paradise</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ">
+                <li class="nav-item">
+                <a class="nav-link text-white" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-white" href="#">SignIn</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-white" href="#">Kaushalya</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="row" style="height: 20px;">
+    </div>
+    <div class="container-fluid">
+         <div class="row">
+             <div class="col-md-2">
+             </div>
+                  <div class="col-md-8">
+                       <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                  <div class="carousel-item active">
+                                       <img src="./images/1.jpg" class="d-block w-100" alt="...">
+                                  </div>
+                                  <div class="carousel-item">
+                                      <img src="./images/2.jpg" class="d-block w-100" alt="...">
+                                  </div>
+                                  <div class="carousel-item">
+                                       <img src="./images/3.jpg" class="d-block w-100" alt="...">
+                                  </div>
+                            </div>
+                       </div>
+                  </div>
+             <div class="col-md-2"></div>
+        </div>
+         <div class="row" style="height: 60px;">
+        </div>
+        <div class="row bg-primary text-white" style="height: 60px;">
+             <div class="col-md-1">          
+             </div>
+             <div class="col-md-4 ">
+                <span class="align-middle" >Properties</span>
+             </div>
+             <div class="col-md-7">
+             </div>
+        </div>
+    </div>
+  </body>
+</html>
