75d960888c93b23f7d45b95f10a30e47bcbd10a5
Kaushalya Harshanath <wakharshanath@gmail.com>
2021-08-16 17:40:21 +0530

    add new template,add php extention,login and logout function implimentation

---
+++ View/HomeBody.php
@@ -0,0 +1,37 @@
+
+    <div class="container-fluid">
+         <div class="row">
+             <div class="col-md-2 bg-primary">
+                    <!--Advertisement images-->
+             </div>
+                  <div class="col-md-8">
+                       <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                  <div class="carousel-item active">
+                                       <img src="./images/1.jpg" class="d-block w-100" alt="...">
+                                  </div>
+                                  <div class="carousel-item">
+                                      <img src="./images/2.jpg" class="d-block w-100" alt="...">
+                                  </div>
+                                  <div class="carousel-item">
+                                       <img src="./images/3.jpg" class="d-block w-100" alt="...">
+                                  </div>
+                            </div>
+                       </div>
+                  </div>
+             <div class="col-md-2 bg-danger">
+                  <!--Advertisement images-->
+             </div>
+        </div>
+         <div class="row" style="height: 60px;">
+        </div>
+        <div class="row bg-primary text-white" style="height: 60px;">
+             <div class="col-md-1">          
+             </div>
+             <div class="col-md-4 ">
+                <span class="align-middle" >Properties</span>
+             </div>
+             <div class="col-md-7">
+             </div>
+        </div>
+    </div>
---
+++ View/LoginBody.php
@@ -0,0 +1,26 @@
+<div class="container-fluid bg">
+  <div class="row" style="height:10vh">
+    
+  </div>
+  <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-8">
+            <div class="w3-container w3-teal">
+            <h2>Input Form</h2>
+            </div>
+
+            <form class="w3-container" method="post" action="./Controller/LoginController.php">
+            <label class="w3-text-teal"><b>User Name</b></label>
+            <input class="w3-input w3-border w3-light-grey" name="username" type="text">
+
+            <label class="w3-text-teal"><b>Password</b></label>
+            <input class="w3-input w3-border w3-light-grey" name="userpassword" type="text">
+
+            <button class="w3-btn w3-blue-grey">Login</button>
+            </form> 
+        </div>
+        <div class="col-md-2">  
+        </div>
+  </div> 
+</div>
